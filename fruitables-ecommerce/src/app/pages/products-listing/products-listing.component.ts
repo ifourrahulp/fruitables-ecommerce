ff23ed60e8ede80b967106260072ea2d6e76e677
RahulKumar <rahul@ifourtechnolab.com>
2024-02-16 14:43:34 +0530

    feat: infinite scroll is added.

--- fruitables-ecommerce/src/app/pages/products-listing/products-listing.component.ts
+++ fruitables-ecommerce/src/app/pages/products-listing/products-listing.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products-listing.component.scss']
 })
 export class ProductsListingComponent implements OnInit {
-
+  isLoading: boolean = false;
   productList = [{
       title: 'Grapes',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt',
@@ -56,11 +56,67 @@ export class ProductsListingComponent implements OnInit {
       imageUrl: 'assets/img/fruite-item-4.jpg'
     },
     {
-      title: 'Orange',
+      title: 'Orange1',
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt',
       price: '$4.99 / kg',
       imageUrl: 'assets/img/fruite-item-1.jpg'
     },
+  ];
+
+  appendProductList = [{
+    title: 'Grapes',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt',
+    price: '$4.99 / kg',
+    imageUrl: 'assets/img/fruite-item-5.jpg'
+  },
+  {
+    title: 'Grapes',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt',
+    price: '$4.99 / kg',
+    imageUrl: 'assets/img/fruite-item-5.jpg'
+  },
+  {
+    title: 'Raspberries',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt',
+    price: '$4.99 / kg',
+    imageUrl: 'assets/img/fruite-item-2.jpg'
+  },
+  {
+    title: 'Apricots',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt',
+    price: '$4.99 / kg',
+    imageUrl: 'assets/img/fruite-item-4.jpg'
+  },
+  {
+    title: 'Banana',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt',
+    price: '$4.99 / kg',
+    imageUrl: 'assets/img/fruite-item-3.jpg'
+  },
+  {
+    title: 'Orange',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt',
+    price: '$4.99 / kg',
+    imageUrl: 'assets/img/fruite-item-1.jpg'
+  },
+  {
+    title: 'Banana',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt',
+    price: '$4.99 / kg',
+    imageUrl: 'assets/img/fruite-item-3.jpg'
+  },
+  {
+    title: 'Apricots',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt',
+    price: '$4.99 / kg',
+    imageUrl: 'assets/img/fruite-item-4.jpg'
+  },
+  {
+    title: 'Orange2',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt',
+    price: '$4.99 / kg',
+    imageUrl: 'assets/img/fruite-item-1.jpg'
+  },
   ]
   number = 0;
   constructor() { }
@@ -69,11 +125,15 @@ export class ProductsListingComponent implements OnInit {
   }
 
   onScroll= ()=>{
-    if(this.number < 5) {
-      let productList = JSON.parse(JSON.stringify(this.productList));
-      this.productList = [...this.productList, ...productList];
-      this.number++;
-    }
+    this.isLoading = true;
+    setTimeout(() => {
+      if(this.number < 5) {
+        this.productList = [...this.productList, ...this.appendProductList];
+        this.number++;
+        this.isLoading = false;
+      }
+    }, 100)
+    
     
   }
 
